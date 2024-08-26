c14b472301cfb97073c8a5217d901a17728ca139
Aleksandr Ryabinskii <olexander.rabota@yandex.ru>
2024-08-26 18:23:00 +0300

    updated requarements.txt and cheked work docker

--- src/db/database.py
+++ src/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from src.config import DB_NAME, DB_HOST, DB_PORT, DB_USER, DB_PASS
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@db:{DB_PORT}/{DB_NAME}" # запуск на pg {DB_HOST}
 engine = create_async_engine(DATABASE_URL)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
